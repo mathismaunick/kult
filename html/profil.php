d868089570f88c2c0c5cbd5a62917365cfcf1f8e
Mathis Maunick <mathis.maunick@gmail.com>
2020-01-21 16:22:44 +0400

    footer modif

--- html/profil.php
+++ html/profil.php
@@ -488,141 +488,138 @@ $bdd = new PDO('mysql:host=127.0.0.1;dbname=kult', 'root', '');
         <!-- =============== START OF FOOTER =============== -->
         <footer class="footer1 bg-dark">
 
-            <!-- ===== START OF FOOTER WIDGET AREA ===== -->
-            <div class="footer-widget-area ptb100">
-                <div class="container">
-                    <div class="row">
+<!-- ===== START OF FOOTER WIDGET AREA ===== -->
+<div class="footer-widget-area ptb100">
+    <div class="container">
+        <div class="row">
 
-                        <!-- Start of Widget 1 -->
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="widget widget-about">
+            <!-- Start of Widget 1 -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget widget-about">
 
-                                <!-- INSERT YOUR LOGO HERE -->
-                                <img src="assets/images/logo.svg" alt="logo" class="logo">
-                                <!-- INSERT YOUR WHITE LOGO HERE -->
-                                <img src="assets/images/logo-white.svg" alt="white logo" class="logo-white">
-                                <p class="nomargin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, ducimus, atque. Praesentium suscipit provident explicabo dignissimos nostrum numquam deserunt earum accusantium et fugit.</p>
-                            </div>
-                        </div>
-                        <!-- End of Widget 1 -->
-
-                        <!-- Start of Widget 2 -->
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="widget widget-links">
-                                <h4 class="widget-title">Useful links</h4>
-
-                                <ul class="general-listing">
-                                    <li><a href="#">about movify</a></li>
-                                    <li><a href="#">blog</a></li>
-                                    <li><a href="#">forum</a></li>
-                                    <li><a href="#">my account</a></li>
-                                    <li><a href="#">watch list</a></li>
-                                </ul>
-
-                            </div>
-                        </div>
-                        <!-- End of Widget 2 -->
-
-                        <!-- Start of Widget 3 -->
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="widget widget-blog">
-                                <h4 class="widget-title">latest news</h4>
-
-                                <ul class="blog-posts">
-                                    <li><a href="#">blog post 1</a><small>januar 13, 2018</small></li>
-                                    <li><a href="#">blog post 2</a><small>januar 13, 2018</small></li>
-                                    <li><a href="#">blog post 3</a><small>januar 13, 2018</small></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- End of Widget 3 -->
-
-                        <!-- Start of Widget 4 -->
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="widget widget-social">
-                                <h4 class="widget-title">follow us</h4>
-
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, ducimus, atque.</p>
-
-                                <!-- Start of Social Buttons -->
-                                <ul class="social-btns">
-                                    <!-- Social Media -->
-                                    <li>
-                                        <a href="#" class="social-btn-roll facebook">
-                                            <div class="social-btn-roll-icons">
-                                                <i class="social-btn-roll-icon fa fa-facebook"></i>
-                                                <i class="social-btn-roll-icon fa fa-facebook"></i>
-                                            </div>
-                                        </a>
-                                    </li>
-
-                                    <!-- Social Media -->
-                                    <li>
-                                        <a href="#" class="social-btn-roll twitter">
-                                            <div class="social-btn-roll-icons">
-                                                <i class="social-btn-roll-icon fa fa-twitter"></i>
-                                                <i class="social-btn-roll-icon fa fa-twitter"></i>
-                                            </div>
-                                        </a>
-                                    </li>
-
-                                    <!-- Social Media -->
-                                    <li>
-                                        <a href="#" class="social-btn-roll google-plus">
-                                            <div class="social-btn-roll-icons">
-                                                <i class="social-btn-roll-icon fa fa-google-plus"></i>
-                                                <i class="social-btn-roll-icon fa fa-google-plus"></i>
-                                            </div>
-                                        </a>
-                                    </li>
-
-                                    <!-- Social Media -->
-                                    <li>
-                                        <a href="#" class="social-btn-roll instagram">
-                                            <div class="social-btn-roll-icons">
-                                                <i class="social-btn-roll-icon fa fa-instagram"></i>
-                                                <i class="social-btn-roll-icon fa fa-instagram"></i>
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <!-- End of Social Buttons -->
-
-                            </div>
-                        </div>
-                        <!-- End of Widget 4 -->
-
-                    </div>
+                    <!-- INSERT YOUR LOGO HERE -->
+                    <!--<img src="assets/images/logo.svg" alt="logo" class="logo"> -->
+                    <!-- INSERT YOUR WHITE LOGO HERE -->
+                    <!--<img src="assets/images/logo-white.svg" alt="white logo" class="logo-white"> -->
+                    <h4 class="widget-title">Kult</h4>
+                    <p class="nomargin">Kult est une plateforme de recommandation de film pour des soirées en groupe. Ne perdez plus votre temps à chercher un film.</p>
                 </div>
             </div>
-            <!-- ===== END OF FOOTER WIDGET AREA ===== -->
+            <!-- End of Widget 1 -->
 
+            <!-- Start of Widget 2 -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget widget-links">
+                    <h4 class="widget-title">liens vers API</h4>
 
-            <!-- ===== START OF FOOTER COPYRIGHT AREA ===== -->
-            <div class="footer-copyright-area ptb30">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="d-flex">
-                                <div class="links">
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item"><a href="#">Privacy & Cookies</a></li>
-                                        <li class="list-inline-item"><a href="#">Terms & Conditions</a></li>
-                                        <li class="list-inline-item"><a href="#">Legal Disclaimer</a></li>
-                                        <li class="list-inline-item"><a href="#">Community</a></li>
-                                    </ul>
+                    <ul class="general-listing">
+                        <li><a href="https://waatch.docs.apiary.io/#">Captain watch</a></li>
+                        <li><a href="https://developers.themoviedb.org/3">The Movie Database</a></li>
+                        <li><a href="http://www.omdbapi.com/">Open Movie Database</a></li>
+                    </ul>
+
+                </div>
+            </div>
+            <!-- End of Widget 2 -->
+
+            <!-- Start of Widget 3 -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget widget-blog">
+                    <h4 class="widget-title">Contact</h4>
+
+                    <ul class="blog-posts">
+                        <li><a href="#">Formulaire de contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End of Widget 3 -->
+
+            <!-- Start of Widget 4 -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget widget-social">
+                    <h4 class="widget-title">Suivez-nous</h4>
+
+                    <p>Vous pouvez nous suivre sur les différents réseaux sociaux.</p>
+
+                    <!-- Start of Social Buttons -->
+                    <ul class="social-btns">
+                        <!-- Social Media -->
+                        <li>
+                            <a href="#" class="social-btn-roll facebook">
+                                <div class="social-btn-roll-icons">
+                                    <i class="social-btn-roll-icon fa fa-facebook"></i>
+                                    <i class="social-btn-roll-icon fa fa-facebook"></i>
                                 </div>
+                            </a>
+                        </li>
 
-                                <div class="copyright ml-auto">All Rights Reserved by <a href="#">Movify</a>.</div>
+                        <!-- Social Media -->
+                        <li>
+                            <a href="#" class="social-btn-roll twitter">
+                                <div class="social-btn-roll-icons">
+                                    <i class="social-btn-roll-icon fa fa-twitter"></i>
+                                    <i class="social-btn-roll-icon fa fa-twitter"></i>
+                                </div>
+                            </a>
+                        </li>
 
-                            </div>
-                        </div>
-                    </div>
+                        <!-- Social Media -->
+                        <li>
+                            <a href="#" class="social-btn-roll google-plus">
+                                <div class="social-btn-roll-icons">
+                                    <i class="social-btn-roll-icon fa fa-google-plus"></i>
+                                    <i class="social-btn-roll-icon fa fa-google-plus"></i>
+                                </div>
+                            </a>
+                        </li>
+
+                        <!-- Social Media -->
+                        <li>
+                            <a href="#" class="social-btn-roll instagram">
+                                <div class="social-btn-roll-icons">
+                                    <i class="social-btn-roll-icon fa fa-instagram"></i>
+                                    <i class="social-btn-roll-icon fa fa-instagram"></i>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- End of Social Buttons -->
+
                 </div>
             </div>
-            <!-- ===== END OF FOOTER COPYRIGHT AREA ===== -->
+            <!-- End of Widget 4 -->
 
-        </footer>
+        </div>
+    </div>
+</div>
+<!-- ===== END OF FOOTER WIDGET AREA ===== -->
+
+
+<!-- ===== START OF FOOTER COPYRIGHT AREA ===== -->
+<div class="footer-copyright-area ptb30">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex">
+                    <div class="links">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><a href="#">Privacy & Cookies</a></li>
+                            <li class="list-inline-item"><a href="#">Terms & Conditions</a></li>
+                            <li class="list-inline-item"><a href="#">Legal Disclaimer</a></li>
+                            <li class="list-inline-item"><a href="#">Community</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="copyright ml-auto">All Rights Reserved by <a href="#">Kult</a>.</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ===== END OF FOOTER COPYRIGHT AREA ===== -->
+
+</footer>
         <!-- =============== END OF FOOTER =============== -->
 
     </div>
---
+++ html/test.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Contact</title>
+</head>
+
+<body>
+<form name="contactform" method="post" action="test2.php">
+<table width="450px">
+<tr>
+ <td valign="top">
+  <label for="first_name">First Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="first_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top"">
+  <label for="last_name">Last Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="last_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="email">Email Address *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="email" maxlength="80" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="telephone">Telephone Number</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="telephone" maxlength="30" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="comments">Comments *</label>
+ </td>
+ <td valign="top">
+  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+ </td>
+</tr>
+<tr>
+ <td colspan="2" style="text-align:center">
+  <input type="submit" value="Submit">   <a href="test2.php">Email Form</a>
+ </td>
+</tr>
+</table>
+</form>
+</body>
+</html>
